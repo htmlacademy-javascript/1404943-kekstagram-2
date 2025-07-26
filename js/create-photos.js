b12eabfac5c47a777988a70cd97dff5a35e2eb72
herold89 <heroldserg89@gmail.com>
2025-07-26 18:34:43 +0300

    отрисовывает фотографии

--- js/create-photos.js
+++ js/create-photos.js
@@ -5,11 +5,11 @@ import {COUNT_PHOTO, DESCRIPTION, MAX_COUNT_LIKES, MIN_COUNT_LIKES} from './data
 const photoId = createIdGenerator();
 const photoUrlId = createRandomIdFromRangeGenerator(1, COUNT_PHOTO);
 
-const createPhotos = () => ({
+const createPhoto = () => ({
   id: photoId(),
   url: `photos/${photoUrlId()}.jpg`,
   description: getRandomArrayIndex(DESCRIPTION),
   likes:getRandomInteger(MIN_COUNT_LIKES, MAX_COUNT_LIKES),
   comments: comments(),
 });
-export const createPhotos = () =>Array.from({length: COUNT_PHOTO}, createPhotos);
+export const createPhotos = (count) =>Array.from({length: count}, createPhoto);
